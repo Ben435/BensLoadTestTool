9576c855a2bf1cc5b2ce5a78d5052990d705f5f8
Ben435 <ben.clare2@gmail.com>
2018-03-08 21:41:09 +1100

    Reformat, build function

--- parabola_test.py
+++ parabola_test.py
@@ -1,13 +1,64 @@
 import time
 from matplotlib import pyplot
+from numpy import arange
+from math import pi, e, sqrt
 
 start = 0
-end = 5 * 60        # 5 mins in seconds.
-peak = 10           # Per second.
+end = 15#5 * 60  # 5 mins in seconds.
+peak = 10  # Per second.
 
 
-def get_eq(x, y, xint1, xint2):
-	return lambda real_x: (y/((x-xint1)*(x-xint2))) * (real_x-xint1)*(real_x-xint2)
+def get_parab(x, y, xint1, xint2):
+    return lambda real_x: (y / ((x - xint1) * (x - xint2))) * (real_x - xint1) * (real_x - xint2)
+
+def get_gauss(a, b, c):
+    return lambda x: a*e**-(((x-b)**2)/2*c**2)
+
+def get_dist_gauss(expected_val, variance):
+    """
+    Distribution style gauss function.
+    :param expected_val: the "u" in the formula's.
+    :param variance: the "o" in the formula's.
+    :return: Function.
+    """
+    return lambda x: (1/(variance*sqrt(2*pi))) * e ** (-(1/2)*((x-expected_val)/variance)**2)
+
+
+def get_built_func(start, acceleration_space, max_height, peak_duration):
+    """
+    Creates an odd semi-gaussian curve with a flat top.
+        __
+       /  \
+    __/    \__
+    :param start: Initial point.
+    :param acceleration_space: Space to get to peak.
+    :param max_height: Peak
+    :param peak_duration: Time at peak.
+    :return: Function that given an x, returns what height it should be at.
+    """
+    # TODO: Make acceleration/deceleration smoother.
+    def func(x):
+        # Out of range.
+        if x < start or x > acceleration_space + peak_duration + acceleration_space:
+            return 0
+        # Up tick.
+        elif x < start + 0.5*(2*start + acceleration_space):
+            return (x-start)**2
+        # Curve to flat
+        elif x < start + acceleration_space:
+            return -(x-(start + acceleration_space))**2 + max_height
+        # Steady peak.
+        elif x < start + acceleration_space + peak_duration:
+            return max_height
+        # Curve down
+        elif x < start + acceleration_space + peak_duration + 0.5*(2*start + acceleration_space):
+            return -(x-(start + acceleration_space + peak_duration))**2 + max_height
+        # Back to flat
+        else:
+            return (x-(start + acceleration_space + peak_duration + acceleration_space))**2
+
+    return func
+
 
 """
 when x=end/2, y=peak    <- Maximum.
@@ -15,14 +66,14 @@ when x=start, y=0   <- y intercept.
 when x=end, y=0     <- x intercepts.
 """
 
-equation = get_eq((end-start)/2, peak, start, end)
+#equation = get_parab((end - start) / 2, peak, start, end)
+#equation = get_gauss(peak, (start + end)/2, 0.1)
+#equation = get_dist_gauss(0, i)
+equation = get_built_func(0, 2, 4, 6)
+
 plot_arr = []
-for i in range(start, end):
-	plot_arr.append(equation(i))
+for j in arange(start, end, 0.1):
+    plot_arr.append(equation(j))
 
-pyplot.plot(list(range(start, end)), plot_arr)
+pyplot.plot(list(arange(start, end, 0.1)), plot_arr)
 pyplot.show()
-
-
-
-
--- tkinter_test.py
+++ tkinter_test.py
@@ -1,4 +1,5 @@
 import matplotlib
+
 matplotlib.use('TkAgg')
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
@@ -24,146 +25,149 @@ runs = 0
 all_points = []
 work_queue = multiprocessing.JoinableQueue()
 time_queue = multiprocessing.Queue()
-#url = "https://swapme.apps.monash.edu"
-#url = "https://monash-swapme-dev.appspot.com"
-#url = "https://monplan.apps.monash.edu"
+# url = "https://swapme.apps.monash.edu"
+# url = "https://monash-swapme-dev.appspot.com"
+# url = "https://monplan.apps.monash.edu"
 url = "http://monplan-api-dev.appspot.com/units/FIT3047"
 
-def animate(i):
-	global all_points
-	while not time_queue.empty():
-		all_points.append(time_queue.get())
-	#all_points = sorted(all_points, key=lambda cur: cur[0])
-	xList = []
-	yList = []
-	y_sum = 0
-	for x, y in all_points:
-		xList.append(x)
-		yList.append(y)
-		y_sum += y
-	if len(yList) > 0:
-		avg_y = round(y_sum/len(yList), 3)
-	else:
-		avg_y = 0
 
-	a.clear()
-	# Re-draw
-	a.set_ylabel("latency (s)")
-	a.set_xlabel("time (s)")
-	a.set_ylim([0, (max(yList) + 1) if len(yList) > 0 else 1])
-	a.set_xlim([0, duration])
-	a.plot(xList, yList, 'bo')
-	a.plot([0, duration], [avg_y, avg_y], 'r-')
+def animate(i):
+    global all_points
+    while not time_queue.empty():
+        all_points.append(time_queue.get())
+    # all_points = sorted(all_points, key=lambda cur: cur[0])
+    xList = []
+    yList = []
+    y_sum = 0
+    for x, y in all_points:
+        xList.append(x)
+        yList.append(y)
+        y_sum += y
+    if len(yList) > 0:
+        avg_y = round(y_sum / len(yList), 3)
+    else:
+        avg_y = 0
+
+    a.clear()
+    # Re-draw
+    a.set_ylabel("latency (s)")
+    a.set_xlabel("time (s)")
+    a.set_ylim([0, (max(yList) + 1) if len(yList) > 0 else 1])
+    a.set_xlim([0, duration])
+    a.plot(xList, yList, 'bo')
+    a.plot([0, duration], [avg_y, avg_y], 'r-')
 
 
 def send_over_time(per_sec, duration, w_queue, t_queue, headers=None, args=None):
-	print("Running {} per second over {} seconds on {}...".format(per_sec, duration, url))
-	pool = []
+    print("Running {} per second over {} seconds on {}...".format(per_sec, duration, url))
+    pool = []
 
-	def make_req(headers=None, args=None):
-		while True:
-			cur = w_queue.get()
-			if cur is None:
-				break
-			s = time.time()
-			try:
-				response = requests.get(url, args, headers=headers)
-				if response.status_code != 200:
-					print("?", end='', flush=True)
-			except ConnectionError:
-				print("!", end='', flush=True)
-			except Exception:
-				print("~", end='', flush=True)
-			finally:
-				end_time = time.time()
-				t_queue.put((round(s-start_time, 3), round(end_time-s, 3)))
-				w_queue.task_done()
+    def make_req(headers=None, args=None):
+        while True:
+            cur = w_queue.get()
+            if cur is None:
+                break
+            s = time.time()
+            try:
+                response = requests.get(url, args, headers=headers)
+                if response.status_code != 200:
+                    print("?", end='', flush=True)
+            except ConnectionError:
+                print("!", end='', flush=True)
+            except Exception:
+                print("~", end='', flush=True)
+            finally:
+                end_time = time.time()
+                t_queue.put((round(s - start_time, 3), round(end_time - s, 3)))
+                w_queue.task_done()
 
-	for i in range(per_sec):
-		t = threading.Thread(target=make_req, args=[headers, args])
-		t.start()
-		pool.append(t)
+    for i in range(per_sec):
+        t = threading.Thread(target=make_req, args=[headers, args])
+        t.start()
+        pool.append(t)
 
-	start_time = time.time()
-	cur = time.time()
-	next_block = cur + (1 / per_sec)
-	end = cur + duration
-	while cur < end:
-		while cur >= next_block:
-			w_queue.put(1)
-			next_block += (1 / per_sec)
-		cur += time.time() - cur
-	# Finish.
-	w_queue.join()
-	for i in range(per_sec):
-		w_queue.put(None)
-	for t in pool:
-		t.join()
+    start_time = time.time()
+    cur = time.time()
+    next_block = cur + (1 / per_sec)
+    end = cur + duration
+    while cur < end:
+        while cur >= next_block:
+            w_queue.put(1)
+            next_block += (1 / per_sec)
+        cur += time.time() - cur
+    # Finish.
+    w_queue.join()
+    for i in range(per_sec):
+        w_queue.put(None)
+    for t in pool:
+        t.join()
 
-	# Get results.
-	times = []
-	while not t_queue.empty():
-		occured_time, cur_time = t_queue.get()
-		times.append([occured_time-start_time, cur_time])
+    # Get results.
+    times = []
+    while not t_queue.empty():
+        occured_time, cur_time = t_queue.get()
+        times.append([occured_time - start_time, cur_time])
 
-	return times
+    return times
 
 
 class GraphDisplay(tkinter.Tk):
-	def __init__(self, *args, **kwargs):
-		tkinter.Tk.__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        tkinter.Tk.__init__(self, *args, **kwargs)
 
-		tkinter.Tk.wm_title(self, "Results Display")
+        tkinter.Tk.wm_title(self, "Results Display")
 
-		container = tkinter.Frame(self)
-		container.pack(side="top", fill="both", expand=True)
-		container.grid_rowconfigure(0, weight=1)
-		container.grid_columnconfigure(0, weight=1)
+        container = tkinter.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-		self.frames = {}
+        self.frames = {}
 
-		for F in [GraphPanel]:
-			frame = F(container, self)
+        for F in [GraphPanel]:
+            frame = F(container, self)
 
-			self.frames[F] = frame
+            self.frames[F] = frame
 
-			frame.grid(row=0, column=0, sticky="nsew")
+            frame.grid(row=0, column=0, sticky="nsew")
 
-		self.show_frame(GraphPanel)
+        self.show_frame(GraphPanel)
 
-	def show_frame(self, cont):
-		frame = self.frames[cont]
-		frame.tkraise()
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
 
 
 class GraphPanel(tkinter.Frame):
-	def __init__(self, parent, controller):
-		self.worker = multiprocessing.Process(target=send_over_time, args=[5, duration, work_queue, time_queue])
-		tkinter.Frame.__init__(self, parent)
-		label = tkinter.Label(self, text="Graph Page!", font=LARGE_FONT)
-		label.pack(pady=10, padx=10)
+    def __init__(self, parent, controller):
+        self.worker = multiprocessing.Process(target=send_over_time, args=[5, duration, work_queue, time_queue])
+        tkinter.Frame.__init__(self, parent)
+        label = tkinter.Label(self, text="Graph Page!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
 
-		def button_func():
-			global all_points
-			if not self.worker.is_alive():
-				all_points = []
-				# Re-create worker.
-				self.worker = multiprocessing.Process(target=send_over_time, args=[rate, duration, work_queue, time_queue])
-				self.worker.start()
-		button1 = ttk.Button(self, text="Run",
-		                     command=button_func)
-		button1.pack()
+        def button_func():
+            global all_points
+            if not self.worker.is_alive():
+                all_points = []
+                # Re-create worker.
+                self.worker = multiprocessing.Process(target=send_over_time,
+                                                      args=[rate, duration, work_queue, time_queue])
+                self.worker.start()
 
-		canvas = FigureCanvasTkAgg(f, self)
-		canvas.show()
-		canvas.get_tk_widget().pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=True)
+        button1 = ttk.Button(self, text="Run",
+                             command=button_func)
+        button1.pack()
 
-		toolbar = NavigationToolbar2TkAgg(canvas, self)
-		toolbar.update()
-		canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
 
 
 if __name__ == "__main__":
-	app = GraphDisplay()
-	ani = animation.FuncAnimation(f, animate, interval=300)
-	app.mainloop()
+    app = GraphDisplay()
+    ani = animation.FuncAnimation(f, animate, interval=300)
+    app.mainloop()
