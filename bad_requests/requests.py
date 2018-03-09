821f8fbf1dfdd7d4613cca2257b7b0cd5fea0503
Ben435 <ben.clare2@gmail.com>
2018-03-09 23:22:13 +1100

    Beginning of worse requests library. Designed to use up way more bandwidth than needed.

---
+++ bad_requests/requests.py
@@ -0,0 +1,115 @@
+import socket
+
+port_options = [80, 443, 8080]
+buffersize = 1024
+standard_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0",
+    "Accept-Language": "en,en-US",
+    "Accept": "text/html,application/xhtml+xml",
+    "Accept-Encoding": 'deflate'
+}
+
+
+class Response:
+    def __init__(self, status_code, status_message, proto, headers, body):
+        self.status_code = status_code
+        self.status_message = status_message
+        self.proto = proto
+        self.headers = headers
+        self.body = body
+
+    def __str__(self):
+        str_headers = "\n".join([": ".join([key, val]) for key, val in self.headers.items()])
+        return "Proto: {}\nCode: {}\nMessage: {}\nHeaders: \"\n{}\"\n\nBody: \"\n{}\"".format(
+            self.proto, self.status_code, self.status_message, str_headers, self.body)
+
+def get(uri, headers=None):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    if uri[-1] == "/":
+        # Strip.
+        url = uri
+        uri = uri[:-1]
+    else:
+        url = uri
+    resource = uri.split("/")
+    # ['http:', '', 'www.example.com', 'what_we_want.html']
+    host = resource[2]
+    if len(resource) < 4:
+        resource = "/".join(resource)
+    else:
+        resource = "/" + "/".join(resource[3:])
+
+    # Join headers.
+    if headers is None:
+        headers = standard_headers
+    header_str = ""
+    for key, val in headers.items():
+        header_str += key + ": " + val + "\n"
+
+    message = "GET {} HTTP/1.1\nHost: {}\n{}\r\n\r\n".format(resource, host, header_str)
+
+    # Connect and send message.
+    worked = False
+    for port in port_options:
+        try:
+            sock.connect((host, port))
+            worked = True
+        except socket.error as e:
+            print(e)
+            print("Failed on port: " + str(port))
+            raise e
+        finally:
+            if worked:
+                break
+    if not worked:
+        print("Failed all ports.")
+        return None
+
+    # Send message.
+    sock.send(message.encode("UTF-8"))
+
+    # Get headers
+    headers = ""
+    received = sock.recv(buffersize)
+    while len(received) == buffersize:
+        headers += received.decode("UTF-8")
+        received = sock.recv(buffersize)
+    headers += received.decode("UTF-8")
+
+    # Parse headers.
+    lines = headers.split("\n")
+    status = lines[0].split(" ")
+    proto = status[0]
+    status_code = status[1]
+    status_message = status[2]
+    dict_headers = {}
+    for line in lines[1:]:        # Skip HTTP/1.1 200 OK line.
+        if len(line) <= 1:
+            continue
+        data = line.split(": ", 1)
+        key = data[0]
+        vals = data[1].strip()
+        dict_headers[key] = vals
+
+    if "Content-Length" in dict_headers:
+        total_body = int(dict_headers["Content-Length"])
+    else:
+        total_body = None
+
+    # Get body.
+    body = ""
+    received = sock.recv(buffersize)
+    total_received = len(received)
+    while total_received < total_body if total_body is not None else len(received) >= buffersize:
+        body += received.decode("UTF-8")
+        received = sock.recv(buffersize)
+        total_received += len(received)
+    body += received.decode("UTF-8")
+
+    return Response(status_code, status_message, proto, dict_headers, body)
+
+
+if __name__ == "__main__":
+    response = get("http://www.theuselessweb.com")
+    print(response)
